15b30020dd774695af237af9df7242ca97690cb3
Sandeep Maurya <108665091+sandeep-mz@users.noreply.github.com>
2023-10-18 18:13:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const byteSize = (str) => {
   // write your code here
+	const blob = new Blob([str]);
+
+  // Get the size property of the Blob to get the byte size
+  return blob.size;
 };
 
 // Do not change the code below
 const str = prompt("Enter some string.");
-alert(byteSize(str));
+alert(byteSize(str)); 
